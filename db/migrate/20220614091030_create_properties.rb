a55132393ef7e00ea615639894c8d0e1854ec0c9
profprogrammeur <contact@profprogrammeur.com>
2022-06-14 14:11:21 +0200

    first commit

---
+++ db/migrate/20220614091030_create_properties.rb
@@ -0,0 +1,12 @@
+class CreateProperties < ActiveRecord::Migration[7.0]
+  def change
+    create_table :properties do |t|
+      t.string :title
+      t.integer :price
+      t.text :description
+      t.text :address
+
+      t.timestamps
+    end
+  end
+end
